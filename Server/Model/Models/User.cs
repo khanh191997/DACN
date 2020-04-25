1f44bdcc7daa2551bc401f6e23a0d9c0bc02de3f
Khanh <khanh.nt191997@gmail.com>
2020-04-25 12:53:55 +0700

    add project Server

---
+++ Server/Model/Models/User.cs
@@ -0,0 +1,48 @@
+namespace Model.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            Carts = new HashSet<Cart>();
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [StringLength(50)]
+        public string Phone { get; set; }
+
+        [StringLength(50)]
+        public string Address { get; set; }
+
+        public DateTime? CreateDate { get; set; }
+
+        public bool? Status { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cart> Carts { get; set; }
+    }
+}
